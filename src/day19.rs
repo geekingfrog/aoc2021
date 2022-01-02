29c85ada98c860d40af3dfd520f24bb198840897
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2022-01-02 20:54:50 +0100

    day19 fast!
    find delta and rotation matrix based on common distance, to minimize
    loops and matrix multiplication.

--- src/day19.rs
+++ src/day19.rs
@@ -5,7 +5,7 @@ use nom::combinator::{map, opt};
 use nom::multi::{count, separated_list1};
 use nom::sequence::{delimited, preceded, terminated, tuple};
 use nom::IResult;
-use std::collections::{BTreeSet, HashSet, VecDeque};
+use std::collections::{BTreeSet, VecDeque};
 use std::ops::Neg;
 
 pub fn solve() -> (usize, i32) {
@@ -143,7 +143,7 @@ fn m3_mul(a: M3, b: M3) -> M3 {
     ]
 }
 
-fn scanner_permutations(scanner: &Scanner) -> [(M3, Vec<Point>); 24] {
+fn rotations() -> impl Iterator<Item = M3> {
     let rots: [M3; 8] = [
         [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
         [[1, 0, 0], [0, 0, -1], [0, 1, 0]],
@@ -163,34 +163,37 @@ fn scanner_permutations(scanner: &Scanner) -> [(M3, Vec<Point>); 24] {
 
     rots.into_iter()
         .cartesian_product(perms)
-        .map(|(a, b)| {
-            let m = m3_mul(a, b);
-            (m, scanner.points.iter().copied().map(|p| p * &m).collect())
-        })
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap()
+        .map(|(a, b)| m3_mul(a, b))
 }
 
-fn overlap(source_points: &[Point], scanner_points: &[Point]) -> Option<Point> {
-    for source_point in source_points {
-        for scanner_point in scanner_points {
-            let delta = source_point - scanner_point;
-            let matching_count = scanner_points
-                .iter()
-                .filter(|scanner_point| {
-                    source_points
-                        .iter()
-                        .any(|source_point| source_point - *scanner_point == delta)
-                })
-                .count();
+/// returns the transformation and delta to apply to `checking` points
+/// so that they are in the same frame of reference
+fn find_delta(sources: &[Point], checking: &[Point], common_dist: i32) -> Option<(M3, Point)> {
+    let (source0, source1) = sources
+        .iter()
+        .tuple_combinations()
+        .find(|(p1, p2)| dist(p1, p2) == common_dist)
+        .unwrap();
+    let (p0, p1) = checking
+        .iter()
+        .tuple_combinations()
+        .find(|(p1, p2)| dist(p1, p2) == common_dist)
+        .unwrap();
 
-            if matching_count >= 12 {
-                return Some(delta);
-            }
+    rotations().find_map(|trans| {
+        let p0 = *p0 * &trans;
+        let p1 = *p1 * &trans;
+        let delta1 = source0 - &p0;
+        let delta2 = source0 - &p1;
+
+        if delta1 == source1 - &p1 || delta1 == p1 - source1 {
+            Some((trans, delta1))
+        } else if delta2 == source1 - &p0 || delta2 == p0 - source1 {
+            Some((trans, delta2))
+        } else {
+            None
         }
-    }
-    None
+    })
 }
 
 fn create_trans_table(scanners: &[Scanner]) -> Vec<(usize, Point, M3)> {
@@ -200,7 +203,7 @@ fn create_trans_table(scanners: &[Scanner]) -> Vec<(usize, Point, M3)> {
     let mut seen = vec![false; n];
     let mut to_check = VecDeque::from([0]);
 
-    let distances: Vec<HashSet<i32>> = scanners
+    let distances: Vec<BTreeSet<i32>> = scanners
         .iter()
         .map(|s| {
             s.points
@@ -222,18 +225,32 @@ fn create_trans_table(scanners: &[Scanner]) -> Vec<(usize, Point, M3)> {
                 continue;
             }
 
-            let has_overlap = distances[source].intersection(&distances[i]).count() >= 66; // 12 * 11 / 2
-            if !has_overlap {
+            let ovs = distances[source].intersection(&distances[i]).collect_vec();
+
+            if ovs.len() < 66 {
+                // 12 choose 2
                 continue;
             }
 
-            for perm in scanner_permutations(&scanners[source]) {
-                if let Some(delta) = overlap(&scanners[i].points, &perm.1) {
-                    to_check.push_back(i);
-                    table[i] = (source, delta, perm.0);
-                    break;
-                }
-            }
+            let candidates = ovs
+                .into_iter()
+                .filter_map(|d| find_delta(&scanners[i].points, &scanners[source].points, *d))
+                // in some unlikely cases, two rotations will give a correct
+                // delta, and so what find_delta returns depends on the order
+                // of the rotations, which isn't ideal.
+                // so take 3, and take two identical
+                .take(3)
+                .collect_vec();
+
+            let (trans, delta) = if candidates[0] == candidates[1] || candidates[0] == candidates[2]
+            {
+                candidates[0]
+            } else {
+                candidates[1]
+            };
+
+            to_check.push_back(i);
+            table[i] = (source, delta, trans);
         }
     }
 
@@ -326,6 +343,7 @@ mod test {
     use super::*;
 
     const TEST_INPUT: &str = include_str!("../resources/day19_test.txt");
+    const REAL_INPUT: &str = include_str!("../resources/day19.txt");
 
     #[test]
     fn test_parser() {
@@ -373,6 +391,13 @@ mod test {
         let puzzle = parse_puzzle(TEST_INPUT);
         let table = create_trans_table(&puzzle);
         assert_eq!(79, solve1(&table, &puzzle));
+    }
+
+    #[test]
+    fn test_solve1_regression() {
+        let puzzle = parse_puzzle(REAL_INPUT);
+        let table = create_trans_table(&puzzle);
+        assert_eq!(451, solve1(&table, &puzzle));
     }
 
     #[test]
